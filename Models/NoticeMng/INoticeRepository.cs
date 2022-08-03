1ebd02a335045a9e6ff52f3c7e84ff8fa47752a3
lsy <2sy96@naver.com>
2022-08-03 17:55:11 +0900

    * 게시판 저장소 진행중

--- Models/NoticeMng/INoticeRepository.cs
+++ Models/NoticeMng/INoticeRepository.cs
@@ -18,7 +18,112 @@ namespace MVC_NotePad.Models.NoticeMng
         /// <summary>
         /// 게시판 쓰기
         /// </summary>
-        /// <param name="notice"></param>
+        /// <param name="notice">게시판</param>
         void WriteNotice(NoticeModel notice);
+
+        /// <summary>
+        /// 게시판 수정하기
+        /// </summary>
+        /// <param name="notice">게시판</param>
+        /// <returns>처리 레코드 수</returns>
+        int UpdateNotice(NoticeModel notice);
+
+        /// <summary>
+        /// 게시판 답변하기
+        /// </summary>
+        /// <param name="notice">게시판</param>
+        void ReplyNotice(NoticeModel notice);
+
+        /// <summary>
+        /// 게시판 삭제하기
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="password">패스워드</param>
+        /// <returns>처리 레코드 수</returns>
+        int DeleteNotice(int id, string password);
+
+        /// <summary>
+        /// 카운트 구하기
+        /// </summary>
+        int GetCount();
+
+        /// <summary>
+        /// 카운트 구하기
+        /// </summary>
+        /// <param name="searchField">검색 필드</param>
+        /// <param name="searchQuery">검색 쿼리</param>
+        /// <returns>카운트</returns>
+        int GetCount(string searchField, string searchQuery);
+
+        /// <summary>
+        /// 게시판 리스트 구하기
+        /// </summary>
+        /// <param name="pageIndex">페이지 인덱스</param>
+        List<NoticeModel> GetNoticeList(int pageIndex);
+
+        /// <summary>
+        /// 게시판 리스트 구하기
+        /// </summary>
+        /// <param name="pageIndex">페이지 인덱스</param>
+        /// <param name="searchField">검색 필드</param>
+        /// <param name="searchQuery">검색 쿼리</param>
+        /// <returns>게시판 리스트</returns>
+        List<NoticeModel> GetNoticeList(int pageIndex, string searchField, string searchQuery);
+
+        /// <summary>
+        /// 게시판 보기
+        /// </summary>
+        NoticeModel GetNotice(int id);
+
+        /// <summary>
+        /// 파일명 구하기
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <returns>파일명</returns>
+        string GetFileName(int id);
+
+        /// <summary>
+        /// 다운로드 카운트 수정하기
+        /// </summary>
+        /// <param name="id">ID</param>
+        void UpdateDownloadCount(int id);
+
+        /// <summary>
+        /// 다운로드 카운트 수정하기
+        /// </summary>
+        /// <param name="fileName">파일명</param>
+        void UpdateDownloadCount(string fileName);
+
+        /// <summary>
+        /// 사진이 있는 최근 게시판 리스트 구하기
+        /// </summary>
+        /// <returns>사진이 있는 최근 게시판 리스트</returns>
+        List<NoticeModel> GetRecentPhotoNoticeList();
+
+        /// <summary>
+        /// 특정 카테고리 최근 게시판 리스트 구하기
+        /// </summary>
+        /// <returns>특정 카테고리 최근 게시판 리스트</returns>
+        List<NoticeModel> GetRecentCategoryNoticeList(string category);
+
+        /// <summary>
+        /// 최근 게시판 리스트 구하기
+        /// </summary>
+        /// <returns>최근 게시판 리스트</returns>
+        List<NoticeModel> GetRecentNoticeList();
+
+        /// <summary>
+        /// 최근 게시판 리스트 구하기
+        /// </summary>
+        /// <param name="noticeCount">게시판 수</param>
+        /// <returns>최근 게시판 리스트</returns>
+        List<NoticeModel> GetRecentNoticeList(int noticeCount);
+
+        /// <summary>
+        /// 게시판 고정하기
+        /// </summary>
+        /// <param name="id"></param>
+        /// <remarks>특정 게시글을 공지사항으로 올리기</remarks>
+        void PinNotice(int id);
     }
 }
---
+++ Models/NoticeMng/NoticeRepository.cs
@@ -0,0 +1,68 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVC_NotePad.Models.NoticeMng
+{
+    public class NoticeRepository : INoticeRepository
+    {
+        /// <summary>
+        /// 구성
+        /// </summary>
+        private IConfiguration configuration;
+
+        /// <summary>
+        /// 로그 기록기
+        /// </summary>
+        private ILogger<NoticeRepository> logger;
+
+        /// <summary>
+        /// 연결
+        /// </summary>
+        private SqlConnection connection;
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        public NoticeRepository(IConfiguration config, ILogger<NoticeRepository> logger)
+        {
+            this.configuration = config;
+            this.connection = new SqlConnection(this.configuration.GetSection("ConnectionStrings").GetSection("DefaultConnection").Value);
+            this.logger = logger;
+        }
+        public int SaveNotice(NoticeModel notice, BoardWriteFormType formType)
+        {
+            int recordCount = 0;
+
+            DynamicParameters dynamicParameters = new DynamicParameters();
+
+            dynamicParameters.Add("@Name", value: notice.Name, dbType: DbType.String);
+            dynamicParameters.Add("@MailAddress", value: notice.MailAddress, dbType: DbType.String);
+            dynamicParameters.Add("@Title", value: notice.Title, dbType: DbType.String);
+            dynamicParameters.Add("@Content", value: notice.Content, dbType: DbType.String);
+            dynamicParameters.Add("@Password", value: notice.Password, dbType: DbType.String);
+            dynamicParameters.Add("@Encoding", value: notice.Encoding, dbType: DbType.String);
+            dynamicParameters.Add("@Homepage", value: notice.Homepage, dbType: DbType.String);
+            dynamicParameters.Add("@FileName", value: notice.FileName, dbType: DbType.String);
+            dynamicParameters.Add("@FileSize", value: notice.FileSize, dbType: DbType.Int32);
+
+            switch (formType)
+            {
+                case BoardWriteFormType.Write:
+                    dynamicParameters.Add("@WriteIP", value: notice.WriteIP, dbType: DbType.String);
+
+                    recordCount = this.connection.Execute("WriteNotice", dynamicParameters, commandType: CommandType.StoredProcedure);
+                    break;
+                case BoardWriteFormType.Modify:
+                    dynamicParameters.Add("@UpdateIP", value: notice.UpdateIP, dbType: DbType.String);
+                    break;
+                case BoardWriteFormType.Reply:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
