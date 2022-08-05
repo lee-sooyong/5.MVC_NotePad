e78a81ad2c15f637d298250a27d5994c5c921d45
lsy <2sy96@naver.com>
2022-08-05 17:50:14 +0900

    * NoticeRepository.cs

--- Models/NoticeMng/NoticeRepository.cs
+++ Models/NoticeMng/NoticeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Dapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -61,15 +62,96 @@ namespace MVC_NotePad.Models.NoticeMng
                     dynamicParameters.Add("@WriteIP", value: notice.WriteIP, dbType: DbType.String);
 
                     recordCount = this.connection.Execute("WriteNotice", dynamicParameters, commandType: CommandType.StoredProcedure);
+
                     break;
+
                 case BoardWriteFormType.Modify:
                     dynamicParameters.Add("@UpdateIP", value: notice.UpdateIP, dbType: DbType.String);
+                    dynamicParameters.Add("@ID", value: notice.ID, dbType: DbType.Int32);
+
+                    recordCount = this.connection.Execute("UpdateNotice", dynamicParameters, commandType: CommandType.StoredProcedure);
+
                     break;
+
                 case BoardWriteFormType.Reply:
-                    break;
-                default:
+                    dynamicParameters.Add("@WriteIP", value: notice.WriteIP, dbType: DbType.String);
+                    dynamicParameters.Add("@ParentID", value: notice.ParentID, dbType: DbType.Int32);
+
+                    recordCount = this.connection.Execute("ReplyNotice", dynamicParameters, commandType: CommandType.StoredProcedure);
+
                     break;
             }
+            return recordCount;
+        }
+
+        /// <summary>
+        /// 게시판 쓰기
+        /// </summary>
+        /// <param name="notice">게시판</param>
+        public void WriteNotice(NoticeModel notice)
+        {
+            this.logger.LogInformation("EXECITE WRITE NOTICE");
+
+            try
+            {
+                SaveNotice(notice, BoardWriteFormType.Write);
+            }
+            catch (Exception e)
+            {
+                this.logger.LogError($"ERROR WRITE NOTICE : {e}");
+            }
+        }
+
+        /// <summary>
+        /// 게시판 수정하기
+        /// </summary>
+        /// <param name="notice">게시판</param>
+        /// <returns>처리 레코드 수</returns>
+        public int UpdateNotice(NoticeModel notice)
+        {
+            int recordCount = 0;
+
+            this.logger.LogInformation("EXECUTE UPDATE NOTICE");
+
+            try
+            {
+                recordCount = SaveNotice(notice, BoardWriteFormType.Modify);
+            }
+            catch (Exception e)
+            {
+                this.logger.LogError($"ERROR EXECUTE UPDATE NOTICE {e}");
+            }
+            return recordCount;
+        }
+
+        /// <summary>
+        /// 게시판 답변하기
+        /// </summary>
+        /// <param name="notice">게시판</param>
+        public void ReplyNotice(NoticeModel notice)
+        {
+            this.logger.LogInformation("EXECUTE REPLY NOTICE");
+
+            try
+            {
+                SaveNotice(notice, BoardWriteFormType.Reply);
+            }
+            catch (Exception e)
+            {
+                this.logger.LogError($"ERROR EXECUTE REPLY NOTICE {e}");
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// 게시판 제하기
+        /// </summary>
+        /// <param name="id">ID</param>
+        /// <param name="password">패스워드</param>
+        /// <returns>처리 레코드 수</returns>
+        public int DeleteNotice(int id, string password)
+        {
+            return this.connection.Execute("DeleteNotice", new { ID = id, Password = password }, commandType: CommandType.StoredProcedure);
         }
     }
 }
